9c8901ec0c419f191afd601e3a25ba12f38ae905
Bulkan Evcimen <bulkan@gmail.com>
2013-09-16 23:16:43 +1000

    use rspec for tests

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module Fare
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -19,5 +20,10 @@ module Fare
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+        g.test_framework :rspec
+    end
+
   end
 end
