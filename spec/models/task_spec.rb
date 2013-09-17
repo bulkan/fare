4d3e70b3ec56e8d63e895b7b436f4596a43ff299
Bulkan Evcimen <bulkan@gmail.com>
2013-09-18 08:35:18 +1000

    simple model validation for description field

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe Task do
-  it 'can be instantiated' do
-    task = Task.new :description => 'wash car'
-    puts task.description
+  before do
+    @task = Task.new(:description => 'wash car')
+  end
+
+  describe'when description field is not present' do
+    before { @task.description = " " }
+    it { should_not be_valid }
   end
 end
